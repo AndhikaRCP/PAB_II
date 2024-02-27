4830fd766554dfd8ca0d30fedfbdd292f6717344
AndhikaRCP <andhikarizky2603@gmail.com>
2024-02-27 11:27:36 +0700

    Pertemuan 3

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ApiService _apiService = ApiService();
-
+// Deklarasi list
   List<Movie> _allMovies = [];
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
